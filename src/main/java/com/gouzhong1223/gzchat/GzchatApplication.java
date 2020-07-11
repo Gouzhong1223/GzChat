07618eb59c986b9b951ce00064609d6b852816ea
Gouzhong1223 <1162864960@qq.com>
2020-07-11 21:45:58 +0200

    init

---
+++ src/main/java/com/gouzhong1223/gzchat/GzchatApplication.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.gzchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GzchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gouzhong1223/gzchat/GzchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.gzchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
